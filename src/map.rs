41a04ab07f01e36eb1dbb8804493884db3399c69
Gabriel Majivu <gmajivu@gmail.com>
2025-01-21 22:25:21 +0300

    render the map

--- src/map.rs
+++ src/map.rs
@@ -22,4 +22,19 @@ impl Map {
             tiles: vec![TileType::Floor; NUM_TILES],
         }
     }
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Wall => {
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+                    }
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    }
+                }
+            }
+        }
+    }
 }
